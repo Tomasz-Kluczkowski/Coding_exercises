4740629d0896da1d5c36d1cc38b1219b182185c9
Tomasz-Kluczkowski <tomaszk1@hotmail.com.uk>
2019-01-10 15:36:39 +0000

    Add 2 stack queue.

--- DATA_STRUCTURES/Queue/queue.py
+++ DATA_STRUCTURES/Queue/queue.py
@@ -15,4 +15,30 @@ class Queue:
     def size(self):
         return len(self.items)
 
-que = Queue()
+
+class Queue2Stacks:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def is_empty(self):
+        return len(self.stack1) == 0
+
+    def size(self):
+        return len(self.stack1)
+
+    def enqueue(self, item):
+        self.stack1.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            while len(self.stack1) > 0:
+                self.stack2.append(self.stack1.pop())
+            top = self.stack2.pop()
+            while len(self.stack2) > 0:
+                self.stack1.append(self.stack2.pop())
+            return top
+        return None
+
+
+
